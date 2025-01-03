59d98cf56ee0357926f9db4ce6cda59236af89b8
shurov <shurov.0.00.0@gmail.com>
2025-01-03 10:03:43 +0600

    fixed build issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from './Router/Router';
+import "./index.css";
 
-const rootElement = document.getElementById('root');
+import { router } from "./Router/Router";
+
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found");
 }
@@ -12,5 +13,5 @@ if (!rootElement) {
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
